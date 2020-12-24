80af0ef3f95888432f27bb082818bb8d70c9b8ba
lhpmain <lhpmain@126.com>
2020-12-24 21:49:07 +0800

    Update getPanIndex.sh

--- docker/getPanIndex.sh
+++ docker/getPanIndex.sh
@@ -2,11 +2,11 @@
 version=""
 if [ "$version" = "" ]
 then
-    version=`curl --silent "https://api.github.com/repos/libsgh/PanIndex/releases/latest" \
+    version=`curl --silent "https://api.github.com/repos/lhpmain/PanIndex/releases/latest" \
         | grep '"tag_name":' \
         | sed -E 's/.*"([^"]+)".*/\1/'`
 fi
-curl -sOL "https://github.com/libsgh/PanIndex/releases/download/${version}/PanIndex-${version}-linux-amd64.tar.gz"
+curl -sOL "https://github.com/lhpmain/PanIndex/releases/download/${version}/PanIndex-${version}-linux-amd64.tar.gz"
 md5sum "PanIndex-"${version}"-linux-amd64.tar.gz"
 tar -xvzf "PanIndex-"${version}"-linux-amd64.tar.gz"
 rm -rf README.md LICENSE CHANGELOG.md
